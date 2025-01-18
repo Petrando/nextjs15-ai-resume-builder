7282d4ef6c9b5ae3646d0118fc0296875a46dac6
Petrando <petrandorichard@gmail.com>
2025-01-18 16:31:34 +0800

    CreateResumeButton UI, use it in resumes page

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,3 +1,14 @@
+import { Metadata } from "next";
+import CreateResumeButton from "./CreateResumeButton";
+
+export const metadata: Metadata = {
+    title: "Your resumes",
+  };
+
 export default function Page(){
-    return <main>Here will be your resume</main>
+    return <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
+            <CreateResumeButton
+                canCreate={true}
+            />
+        </main>
 }
