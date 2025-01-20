0ea7ee590487d996ade7cb744dcdfd3410027ccb
Petrando <petrandorichard@gmail.com>
2025-01-20 12:36:18 +0800

    useEffect to initialize and clean up PersonalInfoForm

--- src/app/(main)/editor/forms/PersonalInfoForm.tsx
+++ src/app/(main)/editor/forms/PersonalInfoForm.tsx
@@ -31,18 +31,16 @@ export default function PersonalInfoForm(/*{
             email: /*resumeData.email || */"",
         },
     });
-
-    /*
+    
     useEffect(() => {
         const { unsubscribe } = form.watch(async (values) => {
-        const isValid = await form.trigger();
-        if (!isValid) return;
-            setResumeData({ ...resumeData, ...values });
+            const isValid = await form.trigger();
+            if (!isValid) return;
+            //setResumeData({ ...resumeData, ...values });
         });
         return unsubscribe;
-    }, [form, resumeData, setResumeData]);
-    */
-
+    }, [form/*, resumeData, setResumeData*/]);
+    
     const photoInputRef = useRef<HTMLInputElement>(null);
 
     return (
@@ -57,64 +55,64 @@ export default function PersonalInfoForm(/*{
                 control={form.control}
                 name="photo"
                 render={({ field: { value, ...fieldValues } }) => (
-                <FormItem>
-                    <FormLabel>Your photo</FormLabel>
-                    <div className="flex items-center gap-2">
-                    <FormControl>
-                        <Input
-                        {...fieldValues}
-                        type="file"
-                        accept="image/*"
-                        onChange={(e) => {
-                            const file = e.target.files?.[0];
-                            fieldValues.onChange(file);
-                        }}
-                        ref={photoInputRef}
-                        />
-                    </FormControl>
-                    <Button
-                        variant="secondary"
-                        type="button"
-                        onClick={() => {
-                        fieldValues.onChange(null);
-                        if (photoInputRef.current) {
-                            photoInputRef.current.value = "";
-                        }
-                        }}
-                    >
-                        Remove
-                    </Button>
-                    </div>
-                    <FormMessage />
-                </FormItem>
+                    <FormItem>
+                        <FormLabel>Your photo</FormLabel>
+                        <div className="flex items-center gap-2">
+                            <FormControl>
+                                <Input
+                                    {...fieldValues}
+                                    type="file"
+                                    accept="image/*"
+                                    onChange={(e) => {
+                                        const file = e.target.files?.[0];
+                                        fieldValues.onChange(file);
+                                    }}
+                                    ref={photoInputRef}
+                                />
+                            </FormControl>
+                            <Button
+                                variant="secondary"
+                                type="button"
+                                onClick={() => {
+                                    fieldValues.onChange(null);
+                                    if (photoInputRef.current) {
+                                        photoInputRef.current.value = "";
+                                    }
+                                }}
+                            >
+                                Remove
+                            </Button>
+                        </div>
+                        <FormMessage />
+                    </FormItem>
                 )}
             />
             <div className="grid grid-cols-2 gap-3">
                 <FormField
-                control={form.control}
-                name="firstName"
-                render={({ field }) => (
-                    <FormItem>
-                    <FormLabel>First name</FormLabel>
-                    <FormControl>
-                        <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                    </FormItem>
-                )}
+                    control={form.control}
+                    name="firstName"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>First name</FormLabel>
+                            <FormControl>
+                                <Input {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
                 />
                 <FormField
-                control={form.control}
-                name="lastName"
-                render={({ field }) => (
-                    <FormItem>
-                    <FormLabel>Last name</FormLabel>
-                    <FormControl>
-                        <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                    </FormItem>
-                )}
+                    control={form.control}
+                    name="lastName"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Last name</FormLabel>
+                            <FormControl>
+                                <Input {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
                 />
             </div>
             <FormField
@@ -132,56 +130,56 @@ export default function PersonalInfoForm(/*{
             />
             <div className="grid grid-cols-2 gap-3">
                 <FormField
-                control={form.control}
-                name="city"
-                render={({ field }) => (
-                    <FormItem>
-                    <FormLabel>City</FormLabel>
-                    <FormControl>
-                        <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                    </FormItem>
-                )}
+                    control={form.control}
+                    name="city"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>City</FormLabel>
+                            <FormControl>
+                                <Input {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
                 />
                 <FormField
-                control={form.control}
-                name="country"
-                render={({ field }) => (
-                    <FormItem>
-                    <FormLabel>Country</FormLabel>
-                    <FormControl>
-                        <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                    </FormItem>
-                )}
+                    control={form.control}
+                    name="country"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Country</FormLabel>
+                            <FormControl>
+                                <Input {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
                 />
             </div>
             <FormField
                 control={form.control}
                 name="phone"
                 render={({ field }) => (
-                <FormItem>
-                    <FormLabel>Phone</FormLabel>
-                    <FormControl>
-                    <Input {...field} type="tel" />
-                    </FormControl>
-                    <FormMessage />
-                </FormItem>
+                    <FormItem>
+                        <FormLabel>Phone</FormLabel>
+                        <FormControl>
+                        <Input {...field} type="tel" />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
                 )}
             />
             <FormField
                 control={form.control}
                 name="email"
                 render={({ field }) => (
-                <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                    <Input {...field} type="email" />
-                    </FormControl>
-                    <FormMessage />
-                </FormItem>
+                    <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                        <Input {...field} type="email" />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
                 )}
             />
             </form>
