db0f7c1d164843d1d8c16e73e401802dd1ad5077
Petrando <petrandorichard@gmail.com>
2025-01-20 16:51:04 +0800

    feat: SkillsForm component

--- src/app/(main)/editor/steps.ts
+++ src/app/(main)/editor/steps.ts
@@ -3,7 +3,7 @@ import { EditorFormProps } from "@/lib/types";
 import EducationForm from "./forms/EducationForm";
 import GeneralInfoForm from "./forms/GeneralInfoForm";
 import PersonalInfoForm from "./forms/PersonalInfoForm";
-//import SkillsForm from "./forms/SkillsForm";
+import SkillsForm from "./forms/SkillsForm";
 //import SummaryForm from "./forms/SummaryForm";
 import WorkExperienceForm from "./forms/WorkExperienceForm";
 
@@ -20,8 +20,8 @@ export const steps: {
         key: "work-experience",
     },
     { title: "Education", component: EducationForm, key: "education" },
-    /*{ title: "Skills", component: SkillsForm, key: "skills" },
-    {
+    { title: "Skills", component: SkillsForm, key: "skills" },
+    /*{
         title: "Summary",
         component: SummaryForm,
         key: "summary",
