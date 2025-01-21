d076538cc20feca7d7175d92081a7a8081771640
Petrando <petrandorichard@gmail.com>
2025-01-21 10:21:08 +0800

    PersonalInfoHeader for ResumePreview component

--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { RefObject, useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import useDimensions from "@/hooks/useDimensions";
 import { cn } from "@/lib/utils";
 import { ResumeValues } from "@/lib/validation";
 
@@ -12,14 +15,96 @@ export default function ResumePreview({
   className,
 }: ResumePreviewProps) {
 
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const { width } = useDimensions(containerRef as RefObject<HTMLDivElement>);
+
   return (
     <div
       className={cn(
         "aspect-[210/297] h-fit w-full bg-white text-black",
         className,
       )}
+      ref={containerRef}
     >
-      <h1>This text should change with the size of container div</h1>
+      <div
+        className={cn("space-y-6 p-6", !width && "invisible")}
+        style={{
+          zoom: (1 / 794) * width,
+        }}        
+        id="resumePreviewContent"
+      >
+      <PersonalInfoHeader resumeData={resumeData} />
+      </div>
+    </div>
+  );
+}
+
+interface ResumeSectionProps {
+  resumeData: ResumeValues;
+}
+
+function PersonalInfoHeader({ resumeData }: ResumeSectionProps) {
+  const {
+    photo,
+    firstName,
+    lastName,
+    jobTitle,
+    city,
+    country,
+    phone,
+    email,
+    colorHex,
+    borderStyle,
+  } = resumeData;
+
+  const [photoSrc, setPhotoSrc] = useState(photo instanceof File ? "" : photo);
+
+  useEffect(() => {
+    const objectUrl = photo instanceof File ? URL.createObjectURL(photo) : "";
+    if (objectUrl) setPhotoSrc(objectUrl);
+    if (photo === null) setPhotoSrc("");
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [photo]);
+
+  return (
+    <div className="flex items-center gap-6">
+      {photoSrc && (
+        <Image
+          src={photoSrc}
+          width={100}
+          height={100}
+          alt="Author photo"
+          className="aspect-square object-cover"          
+        />
+      )}
+      <div className="space-y-2.5">
+        <div className="space-y-1">
+          <p
+            className="text-3xl font-bold"
+            style={{
+              color: colorHex,
+            }}
+          >
+            {firstName} {lastName}
+          </p>
+          <p
+            className="font-medium"
+            style={{
+              color: colorHex,
+            }}
+          >
+            {jobTitle}
+          </p>
+        </div>
+        <p className="text-xs text-gray-500">
+          {city}
+          {city && country ? ", " : ""}
+          {country}
+          {(city || country) && (phone || email) ? " • " : ""}
+          {[phone, email].filter(Boolean).join(" • ")}
+        </p>
+      </div>
     </div>
   );
 }
