378b692f0541483e197641ae1f278370da8a0e30
Petrando <petrandorichard@gmail.com>
2025-01-21 11:11:49 +0800

    feat: ColorPicker to change resume text color

--- src/app/(main)/editor/ResumePreviewSection.tsx
+++ src/app/(main)/editor/ResumePreviewSection.tsx
@@ -3,7 +3,7 @@ import ResumePreview from "@/components/ResumePreview";
 import { cn } from "@/lib/utils";
 import { ResumeValues } from "@/lib/validation";
 //import BorderStyleButton from "./BorderStyleButton";
-//import ColorPicker from "./ColorPicker";
+import ColorPicker from "./ColorPicker";
 
 interface ResumePreviewSectionProps {
   resumeData: ResumeValues;
@@ -20,7 +20,14 @@ export default function ResumePreviewSection({
     <div
       className={cn("group relative hidden w-full md:flex md:w-1/2", className)}
     >
-      
+      <div className="absolute left-1 top-1 flex flex-none flex-col gap-3 opacity-50 transition-opacity group-hover:opacity-100 lg:left-3 lg:top-3 xl:opacity-100">
+        <ColorPicker
+          color={resumeData.colorHex}
+          onChange={(color) =>
+            setResumeData({ ...resumeData, colorHex: color.hex })
+          }
+        />
+      </div>
       <div className="flex w-full justify-center overflow-y-auto bg-secondary p-3">
         <ResumePreview
           resumeData={resumeData}
