148b1b204632f1addc0ef28322b9f3fd57d732eb
Petrando <petrandorichard@gmail.com>
2025-01-21 10:38:13 +0800

    WorkExperienceSection for ResumePreview component

--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import useDimensions from "@/hooks/useDimensions";
 import { cn } from "@/lib/utils";
 import { ResumeValues } from "@/lib/validation";
+import { formatDate } from "date-fns";
 
 interface ResumePreviewProps {
   resumeData: ResumeValues;
@@ -36,6 +37,7 @@ export default function ResumePreview({
       >
       <PersonalInfoHeader resumeData={resumeData} />
       <SummarySection resumeData={resumeData} />
+      <WorkExperienceSection resumeData={resumeData} />
       </div>
     </div>
   );
@@ -133,6 +135,57 @@ function SummarySection({ resumeData }: ResumeSectionProps) {
           Professional profile
         </p>
         <div className="whitespace-pre-line text-sm">{summary}</div>
+      </div>
+    </>
+  );
+}
+
+function WorkExperienceSection({ resumeData }: ResumeSectionProps) {
+  const { workExperiences, colorHex } = resumeData;
+
+  const workExperiencesNotEmpty = workExperiences?.filter(
+    (exp) => Object.values(exp).filter(Boolean).length > 0,
+  );
+
+  if (!workExperiencesNotEmpty?.length) return null;
+
+  return (
+    <>
+      <hr
+        className="border-2"
+        style={{
+          borderColor: colorHex,
+        }}
+      />
+      <div className="space-y-3">
+        <p
+          className="text-lg font-semibold"
+          style={{
+            color: colorHex,
+          }}
+        >
+          Work experience
+        </p>
+        {workExperiencesNotEmpty.map((exp, index) => (
+          <div key={index} className="break-inside-avoid space-y-1">
+            <div
+              className="flex items-center justify-between text-sm font-semibold"
+              style={{
+                color: colorHex,
+              }}
+            >
+              <span>{exp.position}</span>
+              {exp.startDate && (
+                <span>
+                  {formatDate(exp.startDate, "MM/yyyy")} -{" "}
+                  {exp.endDate ? formatDate(exp.endDate, "MM/yyyy") : "Present"}
+                </span>
+              )}
+            </div>
+            <p className="text-xs font-semibold">{exp.company}</p>
+            <div className="whitespace-pre-line text-xs">{exp.description}</div>
+          </div>
+        ))}
       </div>
     </>
   );
