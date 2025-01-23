ff9345692b73781591485999f1fc7eedfda5d0cd
Petrando <petrandorichard@gmail.com>
2025-01-23 12:10:10 +0800

    wrap the (main) layout with SubscriptionLevelProvider

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,8 +1,8 @@
 import PremiumModal from "@/components/premium/PremiumModal";
-//import { getUserSubscriptionLevel } from "@/lib/subscription";
+import { getUserSubscriptionLevel } from "@/lib/subscription";
 import { auth } from "@clerk/nextjs/server";
 import Navbar from "./Navbar";
-//import SubscriptionLevelProvider from "./SubscriptionLevelProvider";
+import SubscriptionLevelProvider from "./SubscriptionLevelProvider";
 
 export default async function Layout({
   children,
@@ -15,13 +15,15 @@ export default async function Layout({
         return null;
     }
 
-    //const userSubscriptionLevel = await getUserSubscriptionLevel(userId);
+    const userSubscriptionLevel = await getUserSubscriptionLevel(userId);
 
     return (        
+      <SubscriptionLevelProvider userSubscriptionLevel={userSubscriptionLevel}>
         <div className="flex min-h-screen flex-col">
             <Navbar />
             {children}
             <PremiumModal />
         </div>
+      </SubscriptionLevelProvider>
     );
 }
